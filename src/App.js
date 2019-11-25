34eee80cbdd1bacb6bc0ef012436c090e3654c25
Brayden Songe <bfsonge@gmail.com>
2019-11-25 16:25:43 -0600

    testing changes on codesandbox

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          bottom text
+          bottom text testing this stuff
         </p>
         <a
           className="App-link"
