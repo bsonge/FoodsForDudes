4b77844854a38a02b20604d683d18db4e7468f84
Brayden Songe <bfsonge@gmail.com>
2019-11-25 16:24:06 -0600

    testing upstream pulling with codesandbox

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          bottom text
+          WOW! LOOK AT THAT STUFF!
         </p>
         <a
           className="App-link"
